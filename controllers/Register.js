699306a9320235c736ee28f2860efcf32568b07b
Your Name <you@example.com>
2023-01-02 11:58:28 +0100

    wrote the code for sending email otp and verifying

--- controllers/Register.js
+++ controllers/Register.js
@@ -3,6 +3,8 @@ const { v4: uuidv4 } = require('uuid');
 const { Op } = require("sequelize")
 const {registerValidation} = require('../validations/joiregistervalidation')
 const {generateemailotp,password_hash} = require('../utils/emailOtp')
+const {sendEmail} = require('../services/emailotp');
+const otp = require('../models/otp');
 
 
 
@@ -20,7 +22,7 @@ const register = async (req,res) => {
     }
         const {fullname,username,password,email} = req.body
         const customer_id = uuidv4();
-        const otp = generateemailotp()
+        const _otp = generateemailotp()
         
 
     try {
@@ -47,33 +49,75 @@ const register = async (req,res) => {
                 password_salt: salt,
                 email:email
             });
-            
         })
-        
+        .then((insertIntoOtpTable) => {
+    
+            return otp.create({
+                
+                otp: _otp,
+                email: email,
+            })
+    
+        })
+        .then((sendOtp)=>{
+            sendEmail(email,'OTP',`Hello ${fullname} This is The Otp Code, Do Not Share With Anyone \n ${_otp}`)
+            
 
-
-
-
-
-
-
-
-
+            res.status(200).json({
+                status:true,
+                message:`Otp has been successfully sent`
+            })
+        })
+        .catch((err)=>{
+            res.status(400).json({
+                status:false,
+                message:err.message
+            })
+        })
     } catch (e) {
         res.status(400).json({
             status:false,
             message:e.message
         })
     }
-    
-
-    
-
-
 }
+const verifyemailOtp = ((req,res)=>{
+
+    const {email,_otp}=req.params
+    try {
+        otp.findAll({
+            where: {
+                [Op.and]: [
+                    {email: email},
+                    {otp:_otp}
+                ]
+            }
+        })
+        .then((data)=>{
+            if (data.length === 0) {
+                res.status(400).json({
+                    status:false,
+                    message:`Incorrect Otp`
+                })
+            }
+        })
+        
 
 
-module.exports = {register}
+
+
+    } catch (err) {
+        res.status(400).json({
+            status: false,
+            message: err.message
+        })
+        
+    }
+        
+})
+
+
+module.exports = {register,verifyemailOtp}
 
     
 

2d81e8dc2759fd1c87635321c17b741668f3bb90
Your Name <you@example.com>
2023-01-30 15:19:33 +0100

    worked on authorization and login

--- controllers/Login.js
+++ controllers/Login.js
@@ -1,13 +1,15 @@
+require('dotenv').config()
 const bcrypt = require('bcrypt')
 const { Op } = require("sequelize")
 const {User} = require('../models')
-const {generateemailotp,password_hash} = require('../utils/emailOtp')
+const jwt = require('jsonwebtoken')
 
 
 
 
 
-const Login =( async (req,res)=>{
+
+const Login = async (req,res)=>{
 
     const {username,password}= req.body
 
@@ -18,25 +20,43 @@ const Login =( async (req,res)=>{
 
         if (result.length === 0) throw new Error("Username or password is incorrect")
 
-        const passwordHashFromDB = User[0].dataValues.password_hash
-        // console.log("pw:",passwordHashFromDB)
-        const passwordCompare =  await bcrypt.compare(password, passwordHashFromDB)
-      
-        if (passwordCompare === false) throw new Error("Username or password is incorrect")
+        //This is to check where you are going to pick the hash password from => console.log(result)
+
+        const hashedpassword = result[0].dataValues.password_hash
+
+        //console.log(hashedpassword)
+
+        const compare = await bcrypt.compare(password, hashedpassword)
+        if (compare === false) {
+            throw new Error("Username or password is incorrect")
+        }
+
+        const token = jwt.sign(
+            {
+                customer_id: result[0].dataValues.customer_id,
+                name: result[0].dataValues.name,
+                email:  result[0].dataValues.email,
+                username: result[0].dataValues.username
+            },
+            process.env.JWT_SECRET,
+            { expiresIn: '1h' }
+        )
+        console.log(token)
+
+        res.setHeader('token', token)
 
         res.status(200).json({
-            status:true,
-            message:`Welcome back`
+            status: true,                               
+            token: token,
+            message: "Sucessfully login"
         })
-
-    
-    } catch (error) {
+    } catch (err) {
         res.status(400).json({
             status:false,
-            message:error.message
+            message:err.message
         })
     }
-})
+}
 
 
 module.exports = {Login}
